0b8db4c43ef5a6d8fb7c112d45e0c3048622a72f
ariel.bonfil <ariel.bonfil@corebiz.ag>
2022-03-16 12:31:26 -0300

    get collection from vtex

--- node/clients/Collections.ts
+++ node/clients/Collections.ts
@@ -3,7 +3,7 @@ import { ExternalClient } from '@vtex/api'
 import { headers } from '../utils/headers'
 
 
-export class Collections extends ExternalClient {
+export default class Collections extends ExternalClient {
 
   constructor(context: IOContext, options?: InstanceOptions) {
       super(`http://${context.account}.vtexcommercestable.com.br`,
@@ -20,7 +20,10 @@ export class Collections extends ExternalClient {
     )
 
   }
-
+  public async getAllCollections(): Promise<any> {
+    console.log("getting collections...")
+    return this.http.getRaw('https://arcencohogareasy.myvtex.com.br/api/catalog_system/pvt/collection/search?page=1&pageSize=20&orderByAsc=true')
+  }
 
 }
 
