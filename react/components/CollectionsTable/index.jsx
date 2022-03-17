655ea5f12ee555c6c7d63b37c050ec9710382090
ariel.bonfil <ariel.bonfil@corebiz.ag>
2022-03-17 18:32:52 -0300

    table of collections

--- react/components/CollectionsTable/index.jsx
+++ react/components/CollectionsTable/index.jsx
@@ -1,13 +1,69 @@
-import React from 'react'
-import { useQuery } from 'react-apollo'
+import React, {useState, useEffect} from 'react'
 
 const CollectionsTable = () => {
 
-  fetch('/_v/collections')
-    .then(response => response.json())
-    .then(response => console.log("datos", response))
-    .catch(err => console.error(err));
+  const [data, setData] = useState({})
+  const [tableKeys, setTableKeys] = useState([])
 
-  return <div>hola</div>
+  useEffect(() => {
+    fetch('/_v/collections')
+    .then(res => res.json())
+    .then(data => {
+      setData(data)
+    })
+    .catch(err => console.error(err));
+  }, [])
+
+  useEffect(() => {
+ if(data[0]){
+  setTableKeys(Object.keys(data[0]))
+ }
+ }, [data])
+
+
+const getDataIntoElementByIndex = (indexOfElement) => {
+  if(indexOfElement)
+  {
+    let elements = Object.entries(data[indexOfElement])
+    console.log("elllements", elements);
+    const getParsedValue = (value) => {
+      if(value){
+        console.log("typeofs", typeof(value));
+        if(typeof(value) === 'string' || typeof(value) === 'number')
+        {
+          return value.toString()
+        }
+        else if (typeof(value) === 'boolean')
+        {
+          return value ? "Si" : "No"
+        }
+        else {
+          return ''
+        }
+      }
+
+    }
+   return elements.map((value, index) => <td>{getParsedValue(value[1])}</td>)
+  }
+}
+
+
+
+  return (
+    <>
+    <table>
+        <thead>
+          <tr>
+          {tableKeys.map((item, index) => <td>{item}</td>)}
+          </tr>
+        </thead>
+        <tbody>
+        {Object.entries(data).map((_, index) => <tr>{getDataIntoElementByIndex(index)}</tr>)}
+        </tbody>
+       </table>
+       </>
+  )
+
+
 }
 export default CollectionsTable
